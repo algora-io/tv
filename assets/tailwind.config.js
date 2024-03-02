9e2a8d18bc0635d8dd23727f29123c1dc6f6df35
zafer <zafer@algora.io>
2024-03-02 17:35:25 +0300

    update tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,32 +1,62 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
+const plugin = require("tailwindcss/plugin");
 const colors = require("tailwindcss/colors");
+
+const gray = {
+  50: "#f8f9fc",
+  100: "#f1f2f9",
+  200: "#e1e2ef",
+  300: "#cbcee1",
+  400: "#9497b8",
+  500: "#65688b",
+  600: "#484b6a",
+  700: "#343756",
+  800: "#1d1e3a",
+  900: "#100f29",
+  950: "#050217",
+};
 
 module.exports = {
   content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
     extend: {
       colors: {
-        white: colors.white,
+        gray,
         green: colors.emerald,
         purple: colors.indigo,
         yellow: colors.amber,
-        gray: {
-          50: "#f8f9fc",
-          100: "#f1f2f9",
-          200: "#e1e2ef",
-          300: "#cbcee1",
-          400: "#9497b8",
-          500: "#65688b",
-          600: "#484b6a",
-          700: "#343756",
-          800: "#1d1e3a",
-          900: "#100f29",
-          950: "#050217",
-        },
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    // Allows prefixing tailwind classes with LiveView classes to add rules
+    // only when LiveView classes are applied, for example:
+    //
+    //     <div class="phx-click-loading:animate-ping">
+    //
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ])
+    ),
+  ],
 };
