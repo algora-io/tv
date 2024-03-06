e68efe6a9867cf4666468154b48b5af5ed16690e
zafer <zafer@algora.io>
2024-03-06 23:41:08 +0300

    delete unused event handlers

--- lib/algora_web/live/home_live.ex
+++ lib/algora_web/live/home_live.ex
@@ -2,7 +2,7 @@ defmodule AlgoraWeb.HomeLive do
   use AlgoraWeb, :live_view
   require Logger
 
-  alias Algora.{Library, Storage}
+  alias Algora.Library
 
   def render(assigns) do
     ~H"""
@@ -22,51 +22,6 @@ defmodule AlgoraWeb.HomeLive do
 
   def handle_params(params, _url, socket) do
     {:noreply, socket |> apply_action(socket.assigns.live_action, params)}
-  end
-
-  def handle_info(
-        {Storage, %Library.Events.ThumbnailsGenerated{video: video}},
-        socket
-      ) do
-    {:noreply,
-     if video.user_id == socket.assigns.channel.user_id do
-       socket
-       |> stream_insert(:videos, video, at: 0)
-     else
-       socket
-     end}
-  end
-
-  def handle_info(
-        {Library, %Library.Events.LivestreamStarted{video: video}},
-        socket
-      ) do
-    %{channel: channel} = socket.assigns
-
-    {:noreply,
-     if video.user_id == channel.user_id do
-       socket
-       |> assign(channel: %{channel | is_live: true})
-       |> stream_insert(:videos, video, at: 0)
-     else
-       socket
-     end}
-  end
-
-  def handle_info(
-        {Library, %Library.Events.LivestreamEnded{video: video}},
-        socket
-      ) do
-    %{channel: channel} = socket.assigns
-
-    {:noreply,
-     if video.user_id == channel.user_id do
-       socket
-       |> assign(channel: %{channel | is_live: false})
-       |> stream_insert(:videos, video)
-     else
-       socket
-     end}
   end
 
   def handle_info({Library, _}, socket), do: {:noreply, socket}
