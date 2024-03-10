d157148132fbcd2c90da08a1c9d066d9aa5d566c
zafer <zafer@algora.io>
2024-03-10 16:55:04 +0300

    use typescript

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,7 @@ config :algora, AlgoraWeb.Endpoint,
 config :esbuild,
   version: "0.12.18",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args: ~w(js/app.ts --bundle --target=es2016 --outdir=../priv/static/assets),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
