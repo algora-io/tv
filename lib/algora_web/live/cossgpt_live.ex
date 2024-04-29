23e220bb4e1caaf21748e1efa49a970926822631
zafer <zafer@algora.io>
2024-04-29 21:14:31 +0300

    delete unused light mode classes

--- lib/algora_web/live/cossgpt_live.ex
+++ lib/algora_web/live/cossgpt_live.ex
@@ -9,16 +9,13 @@ defmodule AlgoraWeb.COSSGPTLive do
     ~H"""
     <div class="text-white min-h-screen max-w-7xl mx-auto">
       <form class="mt-8 max-w-lg mx-auto" phx-submit="search">
-        <label
-          for="default-search"
-          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
+        <label for="default-search" class="mb-2 text-sm font-medium sr-only text-white">
           Search
         </label>
         <div class="relative">
           <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
-              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              class="w-4 h-4 text-gray-400"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -36,13 +33,13 @@ defmodule AlgoraWeb.COSSGPTLive do
           <input
             type="search"
             name="query"
-            class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500 dark:bg-white/[7.5%] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500"
+            class="block w-full p-4 ps-10 text-sm border rounded-lg border-purple-500 bg-white/[7.5%] placeholder-gray-400 text-white ring-purple-500 ring-1"
             placeholder="Search..."
             required
           />
           <button
             type="submit"
-            class="text-white absolute end-2.5 bottom-2.5 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+            class="text-white absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-800"
           >
             Search
           </button>
