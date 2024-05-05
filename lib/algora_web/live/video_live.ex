70f6ab5749ddf6f0d9678403377d8e479a2ce4cb
zafer <zafer@algora.io>
2024-05-05 22:32:17 +0300

    fix sign in button

--- lib/algora_web/live/video_live.ex
+++ lib/algora_web/live/video_live.ex
@@ -335,13 +335,14 @@ defmodule AlgoraWeb.VideoLive do
                       </button>
                     </div>
                   </.simple_form>
-                  <a
-                    :if={!@current_user}
-                    href={Algora.Github.authorize_url()}
-                    class="mt-2 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
-                  >
-                    Sign in to chat
-                  </a>
+                  <div :if={!@current_user} class="lg:pt-2 lg:pb-0 py-4">
+                    <a
+                      href={Algora.Github.authorize_url()}
+                      class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
+                    >
+                      Sign in to chat
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
