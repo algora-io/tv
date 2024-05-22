168cd4e98b9c60a14a6b55bd504335cf1030f9ad
zafer <zafer@algora.io>
2024-05-22 16:42:00 +0300

    ensure user is logged in

--- lib/algora_web/controllers/oauth_controller.ex
+++ lib/algora_web/controllers/oauth_controller.ex
@@ -3,13 +3,15 @@ defmodule AlgoraWeb.OAuthController do
   require Logger
 
   def new(conn, %{"provider" => "restream"} = params) do
-    state = Algora.Util.random_string()
+    if conn.assigns.current_user do
+      state = Algora.Util.random_string()
 
-    # TODO: ensure user is logged in
-
-    conn
-    |> put_session(:user_return_to, params["return_to"])
-    |> put_session(:restream_state, state)
-    |> redirect(external: Algora.Restream.authorize_url(state))
+      conn
+      |> put_session(:user_return_to, params["return_to"])
+      |> put_session(:restream_state, state)
+      |> redirect(external: Algora.Restream.authorize_url(state))
+    else
+      conn |> redirect(to: ~p"/auth/login")
+    end
   end
 end
