de7cba46933a35fab9811d980b6c65bd775392f8
zafer <zafer@algora.io>
2024-05-23 12:14:10 +0300

    add action handlers

--- lib/algora_web_socket.ex
+++ lib/algora_web_socket.ex
@@ -10,57 +10,64 @@ defmodule AlgoraWebSocket do
 
   def handle_frame({:text, msg}, state) do
     case Jason.decode(msg) do
-      {:ok,
-       %{
-         "action" => "event",
-         "payload" => %{
-           "connectionIdentifier" => conn_identifier,
-           "eventPayload" => %{
-             "author" =>
-               %{
-                 "name" => handle,
-                 "displayName" => name,
-                 "avatar" => avatar_url,
-                 "id" => platform_id
-               } = author,
-             "bot" => false,
-             "contentModifiers" => %{"whisper" => false},
-             "text" => body
-           }
-         }
-       }} ->
-        entity =
-          Accounts.get_or_create_entity!(%{
-            name: name,
-            handle: handle,
-            avatar_url: avatar_url,
-            # HACK:
-            platform: String.split(conn_identifier, "-") |> Enum.at(1),
-            platform_id: platform_id,
-            platform_meta: author
-          })
-
-        {:ok, message} = Chat.create_message(entity, state.video, %{body: body})
-
-        # HACK:
-        message = Chat.get_message!(message.id)
-
-        Chat.broadcast_message_sent!(message)
-
-      # TODO: missing data?
-
       {:ok, action} ->
-        Logger.info("Received message: #{inspect(action)}")
+        handle_action(action, state)
 
       {:error, _reason} ->
         Logger.error("Failed to parse message: #{msg}")
+        {:ok, state}
     end
-
-    {:ok, state}
   end
 
   def handle_disconnect(_reason, state) do
     Logger.error("WebSocket disconnected")
     {:reconnect, state}
+  end
+
+  defp handle_action(
+         %{
+           "action" => "event",
+           "payload" => %{
+             "connectionIdentifier" => conn_identifier,
+             "eventPayload" => %{
+               "author" =>
+                 %{
+                   "name" => handle,
+                   "displayName" => name,
+                   "avatar" => avatar_url,
+                   "id" => platform_id
+                 } = author,
+               "bot" => false,
+               "contentModifiers" => %{"whisper" => false},
+               "text" => body
+             }
+           }
+         },
+         state
+       ) do
+    entity =
+      Accounts.get_or_create_entity!(%{
+        name: name,
+        handle: handle,
+        avatar_url: avatar_url,
+        # HACK:
+        platform: String.split(conn_identifier, "-") |> Enum.at(1),
+        platform_id: platform_id,
+        platform_meta: author
+      })
+
+    {:ok, message} = Chat.create_message(entity, state.video, %{body: body})
+
+    # HACK:
+    message = Chat.get_message!(message.id)
+
+    Chat.broadcast_message_sent!(message)
+
+    {:ok, state}
+  end
+
+  defp handle_action(action, state) do
+    Logger.info("Received message: #{inspect(action)}")
+    {:ok, state}
   end
 end
