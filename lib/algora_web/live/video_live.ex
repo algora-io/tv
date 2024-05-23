fe35f627887fb97ce32fad6e0e5881f90694f702
zafer <zafer@algora.io>
2024-05-23 17:38:10 +0300

    update subscribe button

--- lib/algora_web/live/video_live.ex
+++ lib/algora_web/live/video_live.ex
@@ -28,12 +28,28 @@ defmodule AlgoraWeb.VideoLive do
             </path>
             <use href="#b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb" x="86"></use>
           </svg>
-          <blockquote class={[
-            "text-xl px-4 font-semibold leading-8 text-white sm:text-2xl sm:leading-9 line-clamp-2 min-h-[64px] sm:min-h-0",
-            if(@channel.solving_challenge, do: "hidden sm:block")
+          <div class={[
+            "items-center px-4 justify-between gap-4 min-h-[64px] sm:min-h-0",
+            if(@channel.solving_challenge, do: "hidden sm:flex")
           ]}>
-            <p><%= @video.title %></p>
-          </blockquote>
+            <blockquote class={[
+              "text-xl font-semibold leading-8 text-white sm:text-2xl sm:leading-9 line-clamp-2"
+            ]}>
+              <p><%= @video.title %></p>
+            </blockquote>
+            <.button :if={@current_user} phx-click="toggle_subscription">
+              <%= if subscribed?(@current_user, @video) do %>
+                Unsubscribe
+              <% else %>
+                Subscribe
+              <% end %>
+            </.button>
+            <.button :if={!@current_user && @authorize_url}>
+              <.link navigate={@authorize_url}>
+                Subscribe
+              </.link>
+            </.button>
+          </div>
           <div
             :if={@channel.solving_challenge}
             class="-mt-4 block sm:hidden bg-gray-950 text-center py-4"
@@ -228,34 +244,6 @@ defmodule AlgoraWeb.VideoLive do
             </div>
           </div>
         </div>
-        <.button :if={@current_user} phx-click="toggle_subscription">
-          <%= if subscribed?(@current_user, @video) do %>
-            Unsubscribe
-          <% else %>
-            Subscribe
-          <% end %>
-        </.button>
-        <a
-          :if={!@current_user && @authorize_url}
-          href={@authorize_url}
-          class="w-full flex items-center gap-4 justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
-        >
-          <svg
-            width="98"
-            height="96"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 -ml-2"
-            viewBox="0 0 98 96"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-              fill="#fff"
-            />
-          </svg>
-          <span>Sign in to subscribe</span>
-        </a>
       </div>
 
       <aside
