a41347b994cb6fdc1611cae8e23f7889a47f15ef
zafer <zafer@algora.io>
2024-05-24 22:52:37 +0300

    update aesthetics

--- lib/algora_web/live/show_live/show.ex
+++ lib/algora_web/live/show_live/show.ex
@@ -9,7 +9,7 @@ defmodule AlgoraWeb.ShowLive.Show do
   def render(assigns) do
     ~H"""
     <div class="text-white min-h-screen p-8">
-      <div class="flex flex-col md:grid md:grid-cols-3 gap-8">
+      <div class="flex flex-col md:grid md:grid-cols-3 gap-6">
         <div class="md:col-span-1 bg-white/5 ring-1 ring-white/15 rounded-lg p-6 space-y-6">
           <img src={@show.image_url} class="w-[250px] rounded-lg" />
           <div class="space-y-2">
@@ -82,128 +82,130 @@ defmodule AlgoraWeb.ShowLive.Show do
             </div>
           </div>
         </div>
-        <div class="md:col-span-2 bg-white/5 ring-1 ring-white/15 rounded-lg p-6 space-y-6">
-          <div class="flex flex-col md:flex-row md:items-start md:justify-between gap-6">
-            <div>
-              <h1 class="text-4xl font-bold"><%= @show.title %></h1>
-              <div :if={@show.description} class="mt-4 space-y-4">
-                <div>
-                  <h2 class="text-2xl font-bold">About</h2>
-                  <p class="typography whitespace-pre-wrap"><%= @show.description %></p>
+        <div class="md:col-span-2 space-y-6">
+          <div class="bg-white/5 ring-1 ring-white/15 rounded-lg p-6 space-y-6">
+            <div class="flex flex-col md:flex-row md:items-start md:justify-between gap-6">
+              <div>
+                <h1 class="text-4xl font-bold"><%= @show.title %></h1>
+                <div :if={@show.description} class="mt-4 space-y-4">
+                  <div>
+                    <h2 class="text-2xl font-bold">About</h2>
+                    <p class="typography whitespace-pre-wrap"><%= @show.description %></p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div class="space-y-2 w-full max-w-sm">
-              <div :if={@show.scheduled_for} class="bg-gray-950/75 p-4 rounded-lg">
-                <div class="flex items-center justify-between">
-                  <div class="flex items-center space-x-4">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="h-6 w-6 text-green-300"
-                    >
-                      <path d="M8 2v4"></path>
-                      <path d="M16 2v4"></path>
-                      <rect width="18" height="18" x="3" y="4" rx="2"></rect>
-                      <path d="M3 10h18"></path>
-                    </svg>
-                    <div class="shrink-0">
-                      <div class="text-sm font-semibold">
-                        <%= @show.scheduled_for
-                        |> Timex.to_datetime("Etc/UTC")
-                        |> Timex.Timezone.convert("America/New_York")
-                        |> Timex.format!("{WDfull}, {Mshort} {D}") %>
-                      </div>
-                      <div class="text-sm">
-                        <%= @show.scheduled_for
-                        |> Timex.to_datetime("Etc/UTC")
-                        |> Timex.Timezone.convert("America/New_York")
-                        |> Timex.format!("{h12}:{m} {am}, Eastern Time") %>
+              <div class="space-y-2 w-full max-w-sm">
+                <div :if={@show.scheduled_for} class="bg-gray-950/75 p-4 rounded-lg">
+                  <div class="flex items-center justify-between">
+                    <div class="flex items-center space-x-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="h-6 w-6 text-green-300"
+                      >
+                        <path d="M8 2v4"></path>
+                        <path d="M16 2v4"></path>
+                        <rect width="18" height="18" x="3" y="4" rx="2"></rect>
+                        <path d="M3 10h18"></path>
+                      </svg>
+                      <div class="shrink-0">
+                        <div class="text-sm font-semibold">
+                          <%= @show.scheduled_for
+                          |> Timex.to_datetime("Etc/UTC")
+                          |> Timex.Timezone.convert("America/New_York")
+                          |> Timex.format!("{WDfull}, {Mshort} {D}") %>
+                        </div>
+                        <div class="text-sm">
+                          <%= @show.scheduled_for
+                          |> Timex.to_datetime("Etc/UTC")
+                          |> Timex.Timezone.convert("America/New_York")
+                          |> Timex.format!("{h12}:{m} {am}, Eastern Time") %>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <.button :if={@current_user && !@rsvpd?} phx-click="toggle_rsvp">
-                    Attend
-                  </.button>
-                  <.button
-                    :if={@rsvpd?}
-                    disabled
-                    class="bg-green-600 hover:bg-green-600 disabled:opacity-100 text-white flex items-center active:text-white"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="h-5 w-5 -ml-0.5"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l5 5l10 -10" />
-                    </svg>
-                    <span class="ml-1">Attending</span>
-                  </.button>
-                  <.button :if={!@current_user && @authorize_url}>
-                    <.link navigate={@authorize_url}>
+                    <.button :if={@current_user && !@rsvpd?} phx-click="toggle_rsvp">
                       Attend
-                    </.link>
-                  </.button>
-                </div>
-              </div>
-              <.link
-                href={@show.url || ~p"/#{@channel.handle}/latest"}
-                target="_blank"
-                rel="noopener"
-                class="block bg-gray-950/75 p-4 rounded-lg"
-              >
-                <div class="flex items-center justify-between">
-                  <div class="flex items-center space-x-4">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="h-6 w-6 text-red-400"
+                    </.button>
+                    <.button
+                      :if={@rsvpd?}
+                      disabled
+                      class="bg-green-400 hover:bg-green-400 disabled:opacity-100 text-green-950 flex items-center active:text-green-950"
                     >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18.364 19.364a9 9 0 1 0 -12.728 0" /><path d="M15.536 16.536a5 5 0 1 0 -7.072 0" /><path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-                    </svg>
-                    <div>
-                      <div class="text-sm font-semibold">Watch live</div>
-                      <div class="text-sm">
-                        <%= @show.url || "tv.algora.io/#{@channel.handle}/latest" %>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="h-5 w-5 -ml-0.5"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l5 5l10 -10" />
+                      </svg>
+                      <span class="ml-1">Attending</span>
+                    </.button>
+                    <.button :if={!@current_user && @authorize_url}>
+                      <.link navigate={@authorize_url}>
+                        Attend
+                      </.link>
+                    </.button>
+                  </div>
+                </div>
+                <.link
+                  href={@show.url || ~p"/#{@channel.handle}/latest"}
+                  target="_blank"
+                  rel="noopener"
+                  class="block bg-gray-950/75 p-4 rounded-lg"
+                >
+                  <div class="flex items-center justify-between">
+                    <div class="flex items-center space-x-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="h-6 w-6 text-red-400"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18.364 19.364a9 9 0 1 0 -12.728 0" /><path d="M15.536 16.536a5 5 0 1 0 -7.072 0" /><path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                      </svg>
+                      <div>
+                        <div class="text-sm font-semibold">Watch live</div>
+                        <div class="text-sm">
+                          <%= @show.url || "tv.algora.io/#{@channel.handle}/latest" %>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </.link>
+                </.link>
+              </div>
             </div>
           </div>
-
-          <div class="grid grid-cols-1 md:grid-cols-2 gap-6"></div>
-          <h2 class="border-t border-white/15 pt-4 text-2xl font-bold">Past sessions</h2>
-          <div id="past-sessions" class="mt-3 flex gap-8 overflow-x-scroll" phx-update="stream">
-            <div :for={{_id, video} <- @streams.videos} class="max-w-xs shrink-0 w-full">
-              <.link class="cursor-pointer truncate" href={~p"/#{video.channel_handle}/#{video.id}"}>
-                <.video_thumbnail video={video} class="rounded-2xl" />
-                <div class="pt-2 text-base font-semibold truncate"><%= video.title %></div>
-                <div class="text-gray-300 text-sm"><%= Timex.from_now(video.inserted_at) %></div>
-              </.link>
+          <div class="bg-white/5 ring-1 ring-white/15 rounded-lg p-6 space-y-6">
+            <h2 class="pt-4 text-2xl font-bold">Past sessions</h2>
+            <div id="past-sessions" class="mt-3 flex gap-8 overflow-x-scroll" phx-update="stream">
+              <div :for={{_id, video} <- @streams.videos} class="max-w-xs shrink-0 w-full">
+                <.link class="cursor-pointer truncate" href={~p"/#{video.channel_handle}/#{video.id}"}>
+                  <.video_thumbnail video={video} class="rounded-2xl" />
+                  <div class="pt-2 text-base font-semibold truncate"><%= video.title %></div>
+                  <div class="text-gray-300 text-sm"><%= Timex.from_now(video.inserted_at) %></div>
+                </.link>
+              </div>
             </div>
           </div>
         </div>
