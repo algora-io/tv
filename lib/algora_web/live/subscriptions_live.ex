0e8fed9047e51a8a2a7eade03327e2045e02b991
zafer <zafer@algora.io>
2024-05-30 12:35:32 +0300

    update styling

--- lib/algora_web/live/subscriptions_live.ex
+++ lib/algora_web/live/subscriptions_live.ex
@@ -5,7 +5,7 @@ defmodule AlgoraWeb.SubscriptionsLive do
 
   def render(assigns) do
     ~H"""
-    <div class="max-w-4xl mx-auto pt-2 pb-6 px-4 sm:px-6 space-y-6">
+    <div class="max-w-5xl mx-auto pt-2 pb-6 px-4 sm:px-6 space-y-6">
       <.header>
         <h1 class="text-3xl font-semibold">Subscriptions</h1>
         <p class="text-base font-medium text-gray-200">View & manage your subscriptions</p>
@@ -14,18 +14,18 @@ defmodule AlgoraWeb.SubscriptionsLive do
       <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
         <li
           :for={subscription <- @subscriptions}
-          class="col-span-1 flex flex-col divide-y divide-white/20 rounded-2xl overflow-hidden bg-white/5 ring-1 ring-white/20 text-center shadow relative group"
+          class="col-span-1 flex flex-col rounded-2xl overflow-hidden bg-white/5 ring-1 ring-white/20 text-center shadow-lg relative group"
         >
           <img
             class="mx-auto absolute inset-0 flex-shrink-0 object-cover h-full w-full"
             src={subscription.user_avatar_url}
             alt=""
           />
-          <div class="absolute h-full w-full inset-0 bg-gradient-to-b from-transparent to-gray-950/80" />
+          <div class="absolute h-full w-full inset-0 bg-gradient-to-b from-transparent to-80% to-gray-950/80" />
           <div class="absolute inset-0 bg-purple-700/10" />
           <div class="relative">
             <div class="flex flex-1 flex-col min-h-[16rem]">
-              <h3 class="mt-auto text-2xl font-semibold text-white">
+              <h3 class="mt-auto text-2xl font-semibold text-white [text-shadow:#000_10px_5px_10px]">
                 <%= subscription.user_display_name %>
               </h3>
               <dl class="mt-1 flex flex-col justify-between px-2">
@@ -35,7 +35,7 @@ defmodule AlgoraWeb.SubscriptionsLive do
                     subscription.user_meta["user"]["company"] || "@#{subscription.user_handle}" %>
                 </dd>
               </dl>
-              <div class="mt-4 grid grid-cols-2 divide-white/20 divide-x border-t border-transparent group-hover:border-white/20 transition-colors">
+              <div class="-mt-2 group-hover:mt-2 grid grid-cols-2 divide-white/20 divide-x border-t border-transparent group-hover:border-white/20 transition-all">
                 <.button class="opacity-0 rounded-none h-0 group-hover:opacity-100 group-hover:h-10 transition-all text-sm bg-gray-950/50 hover:bg-gray-950/75 text-white">
                   <.link navigate={~p"/#{subscription.user_handle}"} class="relative">
                     Watch
