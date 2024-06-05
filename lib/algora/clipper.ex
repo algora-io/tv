e636f3c84be179d09c73285a925fc9b763c0fb53
zafer <zafer@algora.io>
2024-06-05 23:21:36 +0300

    seek precisely

--- lib/algora/clipper.ex
+++ lib/algora/clipper.ex
@@ -1,22 +1,26 @@
 defmodule Algora.Clipper do
-  alias Algora.Storage
+  alias Algora.{Storage, Library}
 
   defp bucket(), do: Algora.config([:buckets, :media])
 
-  defp to_absolute(uuid, uri), do: "#{Storage.endpoint_url()}/#{bucket()}/#{uuid}/#{uri}"
+  defp to_absolute(:video, uuid, uri),
+    do: "#{Storage.endpoint_url()}/#{bucket()}/#{uuid}/#{uri}"
+
+  defp to_absolute(:clip, uuid, uri),
+    do: "#{Storage.endpoint_url()}/#{bucket()}/clips/#{uuid}/#{uri}"
 
   def clip(video, from, to) do
     playlists = Algora.Admin.get_media_playlists(video)
 
-    timeline =
+    %{timeline: timeline, ss: ss} =
       playlists.video.timeline
-      |> Enum.reduce(%{elapsed: 0, timeline: []}, fn x, acc ->
+      |> Enum.reduce(%{elapsed: 0, ss: 0, timeline: []}, fn x, acc ->
         case x do
           %ExM3U8.Tags.MediaInit{uri: uri} ->
             %{
               acc
               | timeline: [
-                  %ExM3U8.Tags.MediaInit{uri: to_absolute(video.uuid, uri)} | acc.timeline
+                  %ExM3U8.Tags.MediaInit{uri: to_absolute(:video, video.uuid, uri)} | acc.timeline
                 ]
             }
 
@@ -26,12 +30,30 @@ defmodule Algora.Clipper do
           %ExM3U8.Tags.Segment{duration: duration} when acc.elapsed + duration < from ->
             %{acc | elapsed: acc.elapsed + duration}
 
+          %ExM3U8.Tags.Segment{duration: duration, uri: uri}
+          when acc.elapsed < from and acc.elapsed + duration > from ->
+            %{
+              acc
+              | elapsed: acc.elapsed + duration,
+                ss: acc.elapsed + duration - from,
+                timeline: [
+                  %ExM3U8.Tags.Segment{
+                    duration: duration,
+                    uri: to_absolute(:video, video.uuid, uri)
+                  }
+                  | acc.timeline
+                ]
+            }
+
           %ExM3U8.Tags.Segment{duration: duration, uri: uri} ->
             %{
               acc
               | elapsed: acc.elapsed + duration,
                 timeline: [
-                  %ExM3U8.Tags.Segment{duration: duration, uri: to_absolute(video.uuid, uri)}
+                  %ExM3U8.Tags.Segment{
+                    duration: duration,
+                    uri: to_absolute(:video, video.uuid, uri)
+                  }
                   | acc.timeline
                 ]
             }
@@ -40,35 +62,35 @@ defmodule Algora.Clipper do
             acc
         end
       end)
-      |> then(fn clip -> Enum.reverse(clip.timeline) end)
+      |> then(fn clip -> %{ss: clip.ss, timeline: Enum.reverse(clip.timeline)} end)
 
-    %{playlists.video | timeline: timeline}
-  end
-
-  def create_manifest(video, from, to) do
-    "#{clip(video, from, to) |> ExM3U8.serialize()}#EXT-X-ENDLIST\n"
+    %{playlist: %{playlists.video | timeline: timeline}, ss: ss}
   end
 
   def create_clip(video, from, to) do
     uuid = Ecto.UUID.generate()
 
-    manifest = create_manifest(video, from, to)
+    %{playlist: playlist, ss: ss} = clip(video, from, to)
+
+    manifest = "#{ExM3U8.serialize(playlist)}#EXT-X-ENDLIST\n"
 
     {:ok, _} =
-      Storage.upload(manifest, "#{uuid}/g3cFdmlkZW8.m3u8", content_type: "application/x-mpegURL")
+      Storage.upload(manifest, "clips/#{uuid}/g3cFdmlkZW8.m3u8",
+        content_type: "application/x-mpegURL"
+      )
 
     {:ok, _} =
       ExAws.S3.put_object_copy(
         bucket(),
-        "#{uuid}/index.m3u8",
+        "clips/#{uuid}/index.m3u8",
         bucket(),
         "#{video.uuid}/index.m3u8"
       )
       |> ExAws.request()
 
-    timestamp = video.inserted_at |> NaiveDateTime.to_string() |> Slug.slugify()
-    title = Slug.slugify(video.title)
+    url = to_absolute(:clip, uuid, "index.m3u8")
+    filename = Slug.slugify("#{video.title}-#{Library.to_hhmmss(from)}-#{Library.to_hhmmss(to)}")
 
-    "ffmpeg -i \"#{to_absolute(uuid, "index.m3u8")}\" -c copy \"#{timestamp}-#{title}-#{from}-#{to}.mp4\""
+    "ffmpeg -i \"#{url}\" -ss #{ss} -t #{to - from} -c copy \"#{filename}.mp4\""
   end
 end
