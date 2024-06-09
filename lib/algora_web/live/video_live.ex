d8c1744297e14160eef4a44eea4f02134d956eb8
zafer <zafer@algora.io>
2024-06-09 13:33:44 +0300

    misc

--- lib/algora_web/live/video_live.ex
+++ lib/algora_web/live/video_live.ex
@@ -700,7 +700,7 @@ defmodule AlgoraWeb.VideoLive do
     {:noreply, socket |> stream_insert(:messages, message)}
   end
 
-  # def handle_info(_arg, socket), do: {:noreply, socket}
+  def handle_info(_arg, socket), do: {:noreply, socket}
 
   defp fmt(num) do
     chars = num |> Integer.to_string() |> String.to_charlist()
