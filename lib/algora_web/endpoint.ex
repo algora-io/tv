19f94e35709d41ac5c2ce25636ea4d3283a1b6ca
zafer <zafer@algora.io>
2024-07-11 19:22:59 +0300

    add custom websocket handler for chat messages

--- lib/algora_web/endpoint.ex
+++ lib/algora_web/endpoint.ex
@@ -12,6 +12,8 @@ defmodule AlgoraWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  socket "/chat/:channel_handle", AlgoraWeb.Websockets.ChatSocket
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
