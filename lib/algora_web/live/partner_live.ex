4e0081e207f200e0439587e8925ef85ff25b4022
zafer <zafer@algora.io>
2024-07-30 18:16:08 +0300

    add contact form

--- lib/algora_web/live/partner_live.ex
+++ lib/algora_web/live/partner_live.ex
@@ -68,9 +68,6 @@ defmodule AlgoraWeb.PartnerLive do
                 >
                   Get started
                 </a>
-                <a href="#" class="text-sm font-semibold leading-6 text-white">
-                  Live demo <span aria-hidden="true">→</span>
-                </a>
               </div>
             </div>
             <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
@@ -238,6 +235,94 @@ defmodule AlgoraWeb.PartnerLive do
                 </dd>
               </div>
             </dl>
+          </div>
+        </div>
+        <!-- Contact form -->
+        <div class="isolate px-6 py-24 sm:py-32 lg:px-8">
+          <div class="mx-auto relative px-12 py-12 ring-1 ring-purple-300 max-w-2xl rounded-lg shadow-lg overflow-hidden bg-white/5">
+            <div
+              class="absolute inset-x-0 -z-10 transform overflow-hidden blur-3xl"
+              aria-hidden="true"
+            >
+              <div
+                class="relative -z-10 aspect-[1155/678] w-[36.125rem] max-w-none rotate-[30deg] bg-gradient-to-tr from-[#80caff] to-[#4f46e5] opacity-30 sm:w-[72.1875rem]"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+              >
+              </div>
+            </div>
+            <div class="text-center">
+              <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Work with us</h2>
+              <div class="w-64 h-0.5 mx-auto my-4 bg-gradient-to-r from-[#120f22] via-purple-300 to-[#120f22]">
+              </div>
+              <p class="mt-2 text-lg leading-8 text-gray-300">
+                We only partner with 1-2 new clients per month. Your application reaches our CEO's inbox faster than the speed of light.
+              </p>
+            </div>
+            <form action="#" method="POST" class="px-6 pt-8">
+              <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div class="sm:col-span-2">
+                  <label for="email" class="block text-sm font-semibold leading-6 text-white">
+                    What is your email address?
+                  </label>
+                  <div class="mt-2.5">
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      class="block w-full rounded-md border-0 bg-gray-950 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="website" class="block text-sm font-semibold leading-6 text-white">
+                    What is your website?
+                  </label>
+                  <div class="mt-2.5">
+                    <input
+                      type="text"
+                      name="website"
+                      id="website"
+                      class="block w-full rounded-md border-0 bg-gray-950 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                  <label for="revenue" class="block text-sm font-semibold leading-6 text-white">
+                    What is your revenue?
+                  </label>
+                  <div class="mt-2.5">
+                    <input
+                      type="text"
+                      name="revenue"
+                      id="revenue"
+                      class="block w-full rounded-md border-0 bg-gray-950 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div class="sm:col-span-2">
+                  <label for="location" class="block text-sm font-semibold leading-6 text-white">
+                    Where is your company based?
+                  </label>
+                  <div class="mt-2.5">
+                    <input
+                      type="text"
+                      name="location"
+                      id="location"
+                      class="block w-full rounded-md border-0 bg-gray-950 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="mt-10">
+                <button
+                  type="submit"
+                  class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Let's talk
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </main>
