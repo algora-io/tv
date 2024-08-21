6069cc088294d0e8d16a281b704b10e838dec267
zafer <zafer@algora.io>
2024-08-21 20:34:02 +0300

    add testimonial

--- lib/algora_web/live/partner_live.ex
+++ lib/algora_web/live/partner_live.ex
@@ -246,62 +246,240 @@ defmodule AlgoraWeb.PartnerLive do
             </dl>
           </div>
         </div>
-        <!-- Contact form -->
-        <div class="isolate px-6 pt-24 sm:pt-32 pb-12 lg:px-8">
-          <div
-            id="contact-form"
-            class="mx-auto relative px-12 py-12 ring-1 ring-purple-400 max-w-4xl rounded-xl shadow-lg overflow-hidden bg-white/5"
-          >
-            <div
-              class="absolute inset-x-0 -z-10 transform overflow-hidden blur-3xl"
-              aria-hidden="true"
-            >
-              <div
-                class="relative -z-10 aspect-[1155/678] w-[36.125rem] max-w-none rotate-[30deg] bg-gradient-to-tr from-[#80caff] to-[#4f46e5] opacity-10 sm:w-[72.1875rem]"
-                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-              >
-              </div>
-            </div>
-            <div class="text-center">
-              <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl">Work With Us</h2>
-              <div class="w-96 h-0.5 mx-auto my-4 bg-gradient-to-r from-[#120f22] via-purple-400 to-[#120f22]">
-              </div>
-              <p class="mt-2 text-xl sm:text-2xl tracking-tight leading-8 text-gray-300">
-                We only partner with 1-2 new clients per month. Your application reaches our CEO's inbox faster than the speed of light.
+      </main>
+
+      <div class="py-12">
+        <div class="mx-auto max-w-7xl px-4">
+          <div class="mx-auto mt-16 max-w-2xl rounded-3xl bg-white/5 ring-2 ring-purple-500 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+            <div class="p-8 sm:p-10 lg:flex-auto">
+              <h3 class="text-3xl font-bold tracking-tight text-white">
+                Become an Algora TV Partner
+              </h3>
+              <p class="mt-6 text-lg leading-7 text-gray-200">
+                We only partner with 5 devtool companies every month
               </p>
+              <div class="mt-10 flex items-center gap-x-4">
+                <h4 class="flex-none text-base font-semibold leading-6 text-indigo-300">
+                  What's included
+                </h4>
+                <div class="h-px flex-auto bg-indigo-400/40"></div>
+              </div>
+              <ul
+                role="list"
+                class="mt-8 grid grid-cols-1 gap-4 text-base leading-6 text-gray-200 sm:grid-cols-2 sm:gap-6"
+              >
+                <li class="flex gap-x-3">
+                  <svg
+                    class="h-6 w-5 flex-none text-purple-300"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  5 streamer airtime
+                </li>
+                <li class="flex gap-x-3">
+                  <svg
+                    class="h-6 w-5 flex-none text-purple-300"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  50 ad appearances
+                </li>
+                <li class="flex gap-x-3">
+                  <svg
+                    class="h-6 w-5 flex-none text-purple-300"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  5 live reactions
+                </li>
+                <li class="flex gap-x-3">
+                  <svg
+                    class="h-6 w-5 flex-none text-purple-300"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  5 weeks completion
+                </li>
+              </ul>
             </div>
-            <.form for={@form} phx-submit="save" action="#" class="pt-8">
-              <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <div class="sm:col-span-2">
-                  <.input field={@form[:email]} type="email" label="What is your email address?" />
-                </div>
-                <div class="sm:col-span-2">
-                  <.input field={@form[:website_url]} type="text" label="What is your website?" />
-                </div>
-                <div class="sm:col-span-2">
-                  <.input field={@form[:revenue]} type="text" label="What is your revenue?" />
-                </div>
-                <div class="sm:col-span-2">
-                  <.input
-                    field={@form[:company_location]}
-                    type="text"
-                    label="Where is your company based?"
-                  />
+            <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0 h-full">
+              <div class="rounded-2xl bg-gray-900/50 py-10 text-center ring-1 ring-inset ring-white/5 lg:flex lg:flex-col lg:justify-center lg:py-20">
+                <div class="mx-auto max-w-xs px-8">
+                  <p class="text-base font-semibold text-gray-300">Get started</p>
+                  <p class="mt-6 flex items-baseline justify-center gap-x-2">
+                    <span class="text-5xl font-bold tracking-tight text-white">$5,000</span>
+                    <span class="text-sm font-semibold leading-6 tracking-wide text-gray-300">
+                      USD
+                    </span>
+                  </p>
+                  <.link
+                    href="https://cal.com/ioannisflo"
+                    class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Launch campaign
+                  </.link>
                 </div>
               </div>
-              <div class="mt-10">
-                <.button
-                  phx-disable-with="Sending..."
-                  class=" w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-600 active:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-400"
-                >
-                  Let's talk
-                </.button>
-              </div>
-            </.form>
+            </div>
           </div>
         </div>
-      </main>
-      <!-- Footer -->
+      </div>
+
+      <div class="py-12">
+        <div class="mx-auto max-w-7xl px-4">
+          <div class="mx-auto max-w-2xl sm:max-w-5xl">
+            <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl text-center">
+              Get your product in front of millions
+            </h2>
+            <p class="mt-2 text-lg leading-8 text-gray-300 text-center">
+              Reach a highly engaged, tech-focused audience across multiple platforms
+            </p>
+            <div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+              <article class="relative isolate flex flex-col gap-8 lg:flex-row">
+                <div class="relative aspect-video lg:w-[30rem] lg:shrink-0">
+                  <img
+                    src={~p"/images/in-video-ad.png"}
+                    alt=""
+                    class="absolute inset-0 h-full w-full rounded-sm bg-gray-900 object-cover"
+                  />
+                  <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-50/10"></div>
+                </div>
+                <div>
+                  <div class="relative max-w-md">
+                    <h3 class="mt-3 text-2xl font-semibold leading-6 text-white">
+                      <span class="absolute inset-0"></span> In-video ads
+                    </h3>
+                    <p class="mt-5 text-lg leading-6 text-gray-200">
+                      Get your message across during live streams. Simple, effective, and memorable.
+                    </p>
+                  </div>
+                </div>
+              </article>
+              <article class="relative isolate flex flex-col gap-8 lg:flex-row">
+                <div class="relative aspect-video lg:w-[30rem] lg:shrink-0">
+                  <img
+                    src="https://fly.storage.tigris.dev/algora/blurbs/4c519741-476c-4b74-ad50-fa72c2bddb1b.png"
+                    alt=""
+                    class="absolute inset-0 h-full w-full rounded-sm bg-gray-900 object-cover"
+                  />
+                  <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-50/10"></div>
+                </div>
+                <div>
+                  <div class="relative max-w-md">
+                    <h3 class="mt-3 text-2xl font-semibold leading-6 text-white">
+                      <span class="absolute inset-0"></span> Live reactions
+                    </h3>
+                    <p class="mt-5 text-lg leading-6 text-gray-200">
+                      Watch tech streamers try your product live. Real reactions, real engagement.
+                    </p>
+                  </div>
+                </div>
+              </article>
+              <article class="relative isolate flex flex-col gap-8 lg:flex-row">
+                <div class="relative aspect-video lg:w-[30rem] lg:shrink-0">
+                  <img
+                    src={~p"/images/sponsored-stream.png"}
+                    alt=""
+                    class="absolute inset-0 h-full w-full rounded-sm bg-gray-900 object-cover"
+                  />
+                  <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-50/10"></div>
+                </div>
+                <div>
+                  <div class="relative max-w-md">
+                    <h3 class="mt-3 text-2xl font-semibold leading-6 text-white">
+                      <span class="absolute inset-0"></span> Sponsored streams
+                    </h3>
+                    <p class="mt-5 text-lg leading-6 text-gray-200">
+                      Full-length streams showcasing and building with your product. Reach millions across Twitch, YouTube, and X.
+                    </p>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="isolate overflow-hidden px-4">
+        <div class="relative mx-auto max-w-2xl py-12 lg:max-w-5xl">
+          <figure class="grid grid-cols-1 items-center gap-x-6 gap-y-4 lg:gap-x-8">
+            <div class="relative col-span-2 lg:col-start-1 lg:row-start-2">
+              <svg
+                viewBox="0 0 162 128"
+                fill="none"
+                aria-hidden="true"
+                class="absolute -top-12 left-0 -z-10 h-32 stroke-gray-100/10"
+              >
+                <path
+                  id="b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb"
+                  d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z"
+                />
+                <use href="#b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb" x="86" />
+              </svg>
+              <blockquote class="text-xl font-semibold leading-8 text-white sm:text-3xl sm:leading-9">
+                <p>
+                  Every time Tigris partnered with Algora for live media & distribution we saw more developers try Tigris and experienced increased inbound from prospective customers.
+                </p>
+              </blockquote>
+            </div>
+            <div class="col-end-1 w-16 lg:row-span-4 lg:w-56">
+              <img
+                class="rounded-xl lg:rounded-3xl w-full h-full"
+                src="https://avatars.githubusercontent.com/u/1632658?v=4"
+                alt=""
+              />
+            </div>
+            <figcaption class="text-lg sm:text-2xl lg:col-start-1 lg:row-start-3">
+              <div class="flex items-center gap-2">
+                <div class="font-semibold text-white">Ovais Tariq</div>
+                <div class="text-gray-300">/</div>
+                <div class="text-[#7ceec0]">Co-Founder & CEO at Tigris Data</div>
+              </div>
+              <.link
+                target="_blank"
+                rel="noopener"
+                class="mt-2 flex h-full flex-1 text-white no-underline hover:no-underline"
+                href="https://tigrisdata.com"
+              >
+                <img
+                  src="https://assets-global.website-files.com/657988158c7fb30f4d9ef37b/657990b61fd3a5d674cf2298_tigris-logo.svg"
+                  alt="Tigris"
+                  class="h-8 w-auto"
+                />
+              </.link>
+            </figcaption>
+          </figure>
+        </div>
+      </section>
+
       <footer aria-labelledby="footer-heading" class="relative">
         <h2 id="footer-heading" class="sr-only">Footer</h2>
         <div class="mx-auto max-w-7xl px-6 pb-12 pt-4 lg:px-8">
