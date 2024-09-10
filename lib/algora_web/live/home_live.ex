4bdaa42c4ad4ba60bb429ea7f314da21fb9af3bb
zafer <zafer@algora.io>
2024-09-10 06:13:27 +0300

    fix spacing

--- lib/algora_web/live/home_live.ex
+++ lib/algora_web/live/home_live.ex
@@ -526,7 +526,7 @@ defmodule AlgoraWeb.HomeLive do
             <h2 class="text-white text-3xl font-semibold">
               Past livestreams
             </h2>
-            <div class="pr-8 pt-4 gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="pt-4 gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
               <.video_entry :for={video <- @rest_of_videos} video={video} />
             </div>
           </div>
