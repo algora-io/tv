fabb7ca8b8862a2c1f4270302cc2df378a7d54c5
Giles Thompson <giles@papaiti.com>
2024-09-27 09:58:11 +1200

    Create video_thumbnails table

---
+++ priv/repo/migrations/20240926215228_add_video_thumbnails_table.exs
@@ -0,0 +1,15 @@
+defmodule Algora.Repo.Local.Migrations.AddVideoThumbnailsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:video_thumbnails) do
+      add :minutes, :integer, null: false
+      add :thumbnail_url, :string, null: false
+      add :video_id, references(:videos, on_delete: :nothing), null: false
+
+      timestamps()
+    end
+
+    create index(:video_thumbnails, [:video_id])
+  end
+end
