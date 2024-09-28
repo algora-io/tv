ad7aa32764afdcc7400220b446ca1feb27767e94
ty <lastcanal@gmail.com>
2024-09-27 23:02:33 -0400

    Recover pipeline from disconnected rtmp stream
    Upgrade membrane_rtmp_plugin to latest version
    
    Add .tool-versions file
    
    Upgraded Elixir and Erlang minimum versions
    
    elixir 1.17.3-otp-27
    erlang 27.1

--- lib/algora/pipeline/hls.ex
+++ lib/algora/pipeline/hls.ex
@@ -351,11 +351,16 @@ defmodule Algora.Pipeline.HLS do
   defp serialize_regular_segment(%Manifest{} = manifest, segment) do
     time = Ratio.to_float(segment.duration / Time.second())
 
-    Enum.flat_map(segment.attributes, &SegmentAttribute.serialize/1) ++
-      [
-        "#EXTINF:#{time},",
-        "#{storage_base_url()}/#{manifest.video_uuid}/#{segment.name}"
-      ]
+    if time < 0 do
+      IO.puts "SKIPPING SEGMENT #{inspect(segment.duration)}"
+      []
+    else
+      Enum.flat_map(segment.attributes, &SegmentAttribute.serialize/1) ++
+        [
+          "#EXTINF:#{time},",
+          "#{storage_base_url()}/#{manifest.video_uuid}/#{segment.name}"
+        ]
+    end
   end
 
   defp serialize_partial_segments(%Segment{} = segment) do
