75b79bb5c549581630935acf7803c79995db005b
zafer <zafer@algora.io>
2024-10-12 11:51:47 +0300

    merge some live_sessions

--- lib/algora_web/router.ex
+++ lib/algora_web/router.ex
@@ -71,15 +71,10 @@ defmodule AlgoraWeb.Router do
 
     live_session :chat,
       layout: {AlgoraWeb.Layouts, :live_chat},
-      root_layout: {AlgoraWeb.Layouts, :root_embed} do
-      live "/:channel_handle/chat", ChatLive, :show
-      live "/:channel_handle/:video_id/chat", ChatLive, :show
-    end
-
-    live_session :chat_popout,
-      layout: {AlgoraWeb.Layouts, :live_chat},
       root_layout: {AlgoraWeb.Layouts, :root_embed},
       on_mount: [{AlgoraWeb.UserAuth, :current_user}, AlgoraWeb.Nav] do
+      live "/:channel_handle/chat", ChatLive, :show
+      live "/:channel_handle/:video_id/chat", ChatLive, :show
       live "/:channel_handle/:video_id/chat_popout", ChatPopoutLive, :show
     end
   end
